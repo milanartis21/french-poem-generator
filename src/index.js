6c3000f40c14196b1388cede1f77cf51b0ce5c99
milanartis21 <132863051+milanartis21@users.noreply.github.com>
2024-04-17 19:41:29 -0600

    Added API key and the prompt to generate AI response

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-function generatePoem(event) {
-    event.preventDefault();
-
-new Typewriter("#poem", {
-    strings: ['La tombe dit a la rose'],
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
-});
-
+  });
 }
 
-let poemFormElement = document.querySelector(" #poem-generator-form");
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let context =
+    "You are a romantic Poem expert and write short poems. Generate a 3 line poem in basic HTML. Make a line '<br /> after every line'. Follow the user instructions. Do not include a title to the poem. Sign poem using 'Milans AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
